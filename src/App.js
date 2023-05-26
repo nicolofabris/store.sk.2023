65e0379b2f4e110bd35d90ca9a5f61e4f8cc713f
nicolofabris <nicolo.fabriss@gmail.com>
2023-05-26 13:03:54 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import CatNav from './components/catnav/CatNav';
+import TopNav from './components/topnav/TopNav';
+
+function App() {
+  return (
+    <div className="App">
+      <TopNav/>
+      <CatNav/>
+    </div>
+  );
+}
+
+export default App;
